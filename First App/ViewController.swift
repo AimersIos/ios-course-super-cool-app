1ec7fd371e1d6338e6d61e7bdec29a5187132400
Macbook Pro <macbookpro@macbooksmacbook.connectify>
2016-02-26 09:52:31 +0500

    Initial Commit

---
+++ First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by Macbook Pro on 26/02/2016.
+//  Copyright © 2016 Aimer's. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

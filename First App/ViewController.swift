ec9984f6075563ca2685623cf589a45042b955c2
Macbook Pro <macbookpro@Macbooks-MacBook.local>
2016-03-02 11:49:47 +0500

    added files for firstapp

--- First App/ViewController.swift
+++ First App/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  First App
 //
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var coolapp: UIImageView!
+    @IBOutlet weak var coolbg:  UIImageView!
+    @IBOutlet weak var uncoolbutton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makemenotsouncool(sender: AnyObject) {
+        coollapp.hidden = false
+        coolbg.hidden = false
+        uncoolbutton.hidden = true
+        
+    }
 
 }
 
